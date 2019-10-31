02dfb5f252a35ed66a45c3c29945838d3f90aac7
Andrei Stezenko <andrew.stezenko@gmail.com>
2019-10-31 13:41:41 +0300

    Add files via upload

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,34 @@
+jQuery( function( $ ) {
 
+	/**
+	 * When all page is loaded.
+	 */
+	$( document ).ready( function() {
+
+		/**
+		 * Owl Slider.
+		 */
+		if ( $( 'div' ).hasClass( 'cwp-slider' ) ) {
+			var owl = $( '.owl-carousel' );
+			var sliderItems = parseInt( owl.attr( 'data-slides' ) );
+			var sliderTimer = parseInt( owl.attr( 'data-timer' ) );
+			var sliderEffect = owl.attr( 'data-effect' );
+
+			owl.owlCarousel( {
+				items 				: sliderItems,
+		    	animateOut 			: sliderEffect,
+		    	loop 				: true,
+			    margin 				: 0,
+			    nav 				: true,
+			    navText				: ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+			    dots 				: false,
+			    autoplay 			: true,
+			    autoplayHoverPause 	: false,
+			    lazyLoad			: true,
+			    autoplayTimeout		: sliderTimer
+		    } );
+		}
+
+	} );
+
+} );
