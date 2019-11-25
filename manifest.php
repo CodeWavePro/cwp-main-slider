ace12a01b1761970bb64b571cdbe030ba6db9037
Critick <andrew.stezenko@gmail.com>
2019-11-25 18:24:37 +0300

    Add local Owl Carousel 2 files.

--- manifest.php
+++
@@ -1,3 +0,0 @@
-<?php defined( 'FW' ) or die();
-
-$manifest['github_repo'] = 'https://github.com/CodeWavePro/CWP-Slider.git';
--- static.php
+++ static.php
@@ -9,3 +9,36 @@ wp_enqueue_script(
     'fw-shortcode-cwp-main-slider',
     $uri . '/static/js/scripts.min.js'
 );
+
+if ( !is_admin() ) {
+	$uri = fw_get_template_customizations_directory_uri( '/extensions/shortcodes/shortcodes/cwp-main-slider' );
+
+	// Check if Owl Carousel 2 script and styles are registered (enqueued).
+	if ( wp_script_is( 'cwp-owl-carousel-2-script', 'queue' ) ) {
+		return false;
+	}	else {
+		wp_enqueue_script(
+			'cwp-owl-carousel-2-script',
+			$uri . '/static/libs/js/owl.carousel.min.js',
+			array( 'jquery' )
+		);
+	}
+
+	if ( wp_style_is( 'cwp-owl-carousel-2-css', 'queue' ) ) {
+		return false;
+	}	else {
+		wp_enqueue_style(
+			'cwp-owl-carousel-2-css',
+			$uri . '/static/libs/css/owl.carousel.min.css'
+		);
+	}
+
+	if ( wp_style_is( 'cwp-owl-carousel-2-theme-css', 'queue' ) ) {
+		return false;
+	}	else {
+		wp_enqueue_style(
+			'cwp-owl-carousel-2-theme-css',
+			$uri . '/static/libs/css/owl.theme.default.min.css'
+		);
+	}
+}
